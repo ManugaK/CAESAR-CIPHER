65b49fd118ab23dcede24a613e80704b1134af5c
layanga Gunawardana <2022t01544@stu.cmb.ac.lk>
2025-05-06 12:23:30 +0530

    full message processing function added

--- caesar_cipher.h
+++ caesar_cipher.h
@@ -8,4 +8,9 @@ void initQueue();
 char encryptChar(char ch, int key);
 char decryptChar(char ch, int key);
 
+//Full massage processing
+void encrypt(char *message, int key, char *result);
+void decrypt(char *message, int key, char *result);
+
+
 #endif
